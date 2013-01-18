57684d1bb827bae70ab70beb18d5d54df7e18bbd
Slob <xindonger@gmail.com>
2013-01-18 12:16:33 +0800

    hana finish

--- src/package/touhou.h
+++ src/package/touhou.h
@@ -287,7 +287,7 @@ public:
     Q_INVOKABLE ThLiuzhenCard();
 
     virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
 class ThDongmoCard: public SkillCard{
