c09fdd928af895e783487826e2170828a665f055
Slob <xindonger@gmail.com>
2013-01-21 13:03:40 +0800

    fix UUZ

--- src/package/kami.cpp
+++ src/package/kami.cpp
@@ -138,12 +138,164 @@ public:
     }
 };
 
+class ThFanhun: public TriggerSkill{
+public:
+    ThFanhun(): TriggerSkill("thfanhun") {
+        events << AskForPeaches;
+	}
+
+	virtual bool triggerable(const ServerPlayer *target) const{
+        return target != NULL && TriggerSkill::triggerable(target);
+    }
+
+	virtual bool trigger(TriggerEvent, Room* room, ServerPlayer *player, QVariant &data) const{
+        DyingStruct dying_data = data.value<DyingStruct>();
+        if(dying_data.who != player)
+            return false;
+
+        if(player->askForSkillInvoke(objectName(), data)){
+            room->broadcastSkillInvoke(objectName());
+
+            if(player->isChained()){
+                if(dying_data.damage == NULL || dying_data.damage->nature == DamageStruct::Normal)
+                    room->setPlayerProperty(player, "chained", false);
+            }
+            if(!player->faceUp())
+                player->turnOver();
+
+            room->setPlayerProperty(player, "hp", qMin(1, player->getMaxHp()));
+			player->gainMark("@yingxiao");
+			if (player->getMark("@yingxiao") >= 4 && player->hasSkill("thmanxiao"))
+			{
+				LogMessage log;
+				log.type = "#TriggerSkill";
+				log.from = player;
+				log.arg = "thmanxiao";
+				room->sendLog(log);
+				player->getRoom()->killPlayer(player, NULL);
+			}
+        }
+
+        return false;
+    }
+};
+
+class ThYoushang: public TriggerSkill{
+public:
+	ThYoushang(): TriggerSkill("thyoushang"){
+		events << DamageCaused;
+	}
+
+	virtual bool trigger(TriggerEvent, Room* room, ServerPlayer *player, QVariant &data) const{
+        DamageStruct damage = data.value<DamageStruct>();
+		if (damage.card && damage.card->isKindOf("Slash")
+			&& damage.card->isRed() && !damage.chain && !damage.transfer
+			&& player->askForSkillInvoke(objectName()))
+		{
+			room->loseMaxHp(damage.to);
+			player->gainMark("@yingxiao");
+			if (player->getMark("@yingxiao") >= 4 && player->hasSkill("thmanxiao"))
+			{
+				LogMessage log;
+				log.type = "#TriggerSkill";
+				log.from = player;
+				log.arg = "thmanxiao";
+				room->sendLog(log);
+				player->getRoom()->killPlayer(player, NULL);
+			}
+
+			return true;
+        }
+
+        return false;
+    }
+};
+
+ThYouyaCard::ThYouyaCard(){
+}
+
+bool ThYouyaCard::targetFilter(const QList<const Player *> &, const Player *, const Player *) const{
+	return true;
+}
+
+bool ThYouyaCard::targetsFeasible(const QList<const Player *> &targets, const Player *Self) const{
+	return targets.length() == Self->getMark("@yingxiao");
+}
+
+void ThYouyaCard::onEffect(const CardEffectStruct &effect) const{
+	Room *room = effect.from->getRoom();
+	if (!room->askForCard(effect.to, "jink", "@thyouya-jink:" + effect.from->objectName(), QVariant(), Card::MethodResponse, effect.from))
+		effect.from->drawCards(1);
+}
+
+class ThYouyaViewAsSkill: public OneCardViewAsSkill{
+public:
+    ThYouyaViewAsSkill():OneCardViewAsSkill("thyouya"){
+
+    }
+
+    virtual bool isEnabledAtPlay(const Player *) const{
+        return false;
+    }
+
+    virtual bool isEnabledAtResponse(const Player *, const QString &pattern) const{
+        return pattern == "@@thyouya";
+    }
+
+    virtual bool viewFilter(const Card* to_select) const{
+        return true;
+    }
+
+    virtual const Card *viewAs(const Card *originalCard) const{
+        ThYouyaCard *card = new ThYouyaCard;
+        card->addSubcard(originalCard->getId());
+        return card;
+    }
+};
+
+class ThYouya: public TriggerSkill{
+public:
+    ThYouya(): TriggerSkill("thyouya"){
+		events << DamageInflicted;
+        view_as_skill = new ThYouyaViewAsSkill;
+    }
+
+	virtual bool trigger(TriggerEvent, Room* room, ServerPlayer *player, QVariant &) const{
+		if (player->getMark("@yingxiao") > 0)
+			room->askForUseCard(player, "@@thyouya", "@thyouya");
+
+        return false;
+    }
+};
+
+class ThManxiao: public MaxCardsSkill{
+public:
+	ThManxiao(): MaxCardsSkill("thmanxiao"){
+	}
+
+	virtual int getExtra(const Player *target) const{
+		if (target->hasSkill(objectName()))
+			return target->getMark("@yingxiao");
+		else
+			return false;
+	}
+};
+
 KamiPackage::KamiPackage()
     :Package("kami")
 {
+
 	General *kami002 = new General(this, "kami002", "god");
 	kami002->addSkill(new ThJiguang);
 	kami002->addSkill(new ThJiguangBuff);
+
+	General *kami007 = new General(this, "kami007", "god", 2, false);
+	kami007->addSkill(new ThFanhun);
+	kami007->addSkill(new ThYoushang);
+	kami007->addSkill(new ThYouya);
+	kami007->addSkill(new ThManxiao);
+
+    addMetaObject<ThYouyaCard>();
 
 	skills << new ThJiguangDistanceSkill << new ThJiguangGivenSkill;
 }
--- src/package/kami.h
+++ src/package/kami.h
@@ -11,4 +11,15 @@ public:
     KamiPackage();
 };
 
+class ThYouyaCard: public SkillCard{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE ThYouyaCard();
+	
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
 #endif // KAMI_H
