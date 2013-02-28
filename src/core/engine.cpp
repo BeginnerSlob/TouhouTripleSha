abe468f2731043b29adfb6b0ed6fbdd4c7d4114f
Slob <BeginnerSlob@hotmail.com>
2013-02-28 23:47:03 +0800

    update ts file

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -458,7 +458,7 @@ SkillCard *Engine::cloneSkillCard(const QString &name) const{
 }
 
 QString Engine::getVersionNumber() const{
-    return "0217(V0992)";
+    return "0228(V100)";
 }
 
 QString Engine::getVersion() const{
