0e7460849eec3269a8ab70127c69f738849068a3
BeginnerSlob <BeginnerSlob@gmail.com>
2014-06-24 17:28:07 +0800

    陆逊-旧/陆逊-国->间桐樱 连营-旧->无竭 谦逊-旧->（移除） 度势->（修改）->渊壑

--- src/package/hegemony.h
+++ src/package/hegemony.h
@@ -10,16 +10,14 @@ public:
     HegemonyPackage();
 };
 
-class DuoshiCard: public SkillCard {
+class IkYuanheCard: public SkillCard {
     Q_OBJECT
 
 public:
-    Q_INVOKABLE DuoshiCard();
+    Q_INVOKABLE IkYuanheCard();
 
-    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
     virtual void onUse(Room *room, const CardUseStruct &card_use) const;
-    virtual void onEffect(const CardEffectStruct &effect) const;
+    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
 class FenxunCard: public SkillCard {
