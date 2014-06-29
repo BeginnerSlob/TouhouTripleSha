f5c61dc10c7244e2b82a9dfb1f416457aef9a7f0
BeginnerSlob <BeginnerSlob@gmail.com>
2014-06-29 10:11:52 +0800

    张昭＆张纮->琥珀＆翡翠 直谏->羁绊 固政->（修改）->箕箒

--- src/package/mountain.h
+++ src/package/mountain.h
@@ -27,11 +27,11 @@ public:
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
-class ZhijianCard: public SkillCard {
+class IkJibanCard: public SkillCard {
     Q_OBJECT
 
 public:
-    Q_INVOKABLE ZhijianCard();
+    Q_INVOKABLE IkJibanCard();
 
     virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
     virtual void onEffect(const CardEffectStruct &effect) const;
