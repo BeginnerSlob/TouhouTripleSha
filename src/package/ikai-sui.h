ddc920bbfc154c22f51ab5d516d329823b33aafb
BeginnerSlob <BeginnerSlob@gmail.com>
2014-07-03 09:40:32 +0800

    关银屏->玲•海华兹 血祭->血轮 虎啸->（修改）->残月 武继->九命

--- src/package/ikai-sui.h
+++ src/package/ikai-sui.h
@@ -11,4 +11,14 @@ public:
     IkaiSuiPackage();
 };
 
+class IkXielunCard: public SkillCard {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE IkXielunCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
+};
+
 #endif // IKAISUI_H
