a2cd58eaadbb2d3206bff6d39098c166f66bc617
BeginnerSlob <BeginnerSlob@gmail.com>
2014-07-05 08:22:44 +0800

    刘协->食蜂操祈 天命->（修改）->衡谋 密诏->心操

--- src/package/assassins.h
+++ src/package/assassins.h
@@ -13,15 +13,5 @@ public:
     AssassinsPackage();
 };
 
-class MizhaoCard: public SkillCard {
-    Q_OBJECT
-
-public:
-    Q_INVOKABLE MizhaoCard();
-
-    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual void onEffect(const CardEffectStruct &effect) const;
-};
-
 #endif
 
