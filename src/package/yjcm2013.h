b46ca1247de514e2982dedd0888e036382063572
BeginnerSlob <BeginnerSlob@gmail.com>
2014-07-07 00:16:20 +0800

    刘封->柏崎星奈 陷嗣->陷瑜

--- src/package/yjcm2013.h
+++ src/package/yjcm2013.h
@@ -19,27 +19,6 @@ public:
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
-class XiansiCard: public SkillCard {
-    Q_OBJECT
-
-public:
-    Q_INVOKABLE XiansiCard();
-
-    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual void onEffect(const CardEffectStruct &effect) const;
-};
-
-class XiansiSlashCard: public SkillCard {
-    Q_OBJECT
-
-public:
-    Q_INVOKABLE XiansiSlashCard();
-
-    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
-    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual const Card *validate(CardUseStruct &cardUse) const;
-};
-
 class ZongxuanCard: public SkillCard {
     Q_OBJECT
 
