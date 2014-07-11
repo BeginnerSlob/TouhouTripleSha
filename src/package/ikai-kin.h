f5f35fcd2c889a5cdf20c6713a107b977dcd07fc
BeginnerSlob <BeginnerSlob@gmail.com>
2014-07-11 23:35:18 +0800

    陈群->莉亚丝•吉蒙里 定品->定品 法恩->魔弈

--- src/package/ikai-kin.h
+++ src/package/ikai-kin.h
@@ -137,6 +137,16 @@ public:
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
+class IkDingpinCard: public SkillCard {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE IkDingpinCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
 class IkGuanjuCard: public SkillCard {
     Q_OBJECT
 
