943aa4d0d32178e7a69642aa0820959d51009dd0
BeginnerSlob <BeginnerSlob@gmail.com>
2014-07-14 22:33:32 +0800

    bloom045新子憧

--- src/package/ikai-ka.h
+++ src/package/ikai-ka.h
@@ -72,4 +72,14 @@ public:
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
+class IkDengpoCard: public SkillCard {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE IkDengpoCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
 #endif // IKAIKA_H
