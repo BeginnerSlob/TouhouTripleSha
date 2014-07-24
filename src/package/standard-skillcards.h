3ea6e4a1051a38e1e15587aa26cdb51c82b6d952
BeginnerSlob <BeginnerSlob@gmail.com>
2014-07-24 13:16:38 +0800

    华佗->久远寺有珠 急救->回春 除疠->药割

--- src/package/standard-skillcards.h
+++ src/package/standard-skillcards.h
@@ -13,16 +13,6 @@ public:
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
-class ChuliCard: public SkillCard {
-    Q_OBJECT
-
-public:
-    Q_INVOKABLE ChuliCard();
-
-    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
-};
-
 class YijiCard: public SkillCard {
     Q_OBJECT
 
