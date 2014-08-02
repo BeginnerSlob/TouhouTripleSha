921f99e0f9ebca22d65e29e60d8d40b379cc346a
BeginnerSlob <BeginnerSlob@gmail.com>
2014-08-02 16:42:45 +0800

    LUNA045-千堂瑛里华

--- src/package/ikai-ka.h
+++ src/package/ikai-ka.h
@@ -184,4 +184,18 @@ public:
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
+class IkXiekeCard: public SkillCard {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE IkXiekeCard();
+
+    virtual bool targetFixed() const;
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+
+    virtual const Card *validate(CardUseStruct &card_use) const;
+    virtual const Card *validateInResponse(ServerPlayer *user) const;
+};
+
 #endif // IKAIKA_H
