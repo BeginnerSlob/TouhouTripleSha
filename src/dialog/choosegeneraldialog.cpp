fa952c97bad7ca8cc0b0e6a9fefadf26977b836a
BeginnerSlob <BeginnerSlob@gmail.com>
2014-10-04 06:13:46 +0800

    fix a crash?

--- src/dialog/choosegeneraldialog.cpp
+++ src/dialog/choosegeneraldialog.cpp
@@ -82,7 +82,9 @@ ChooseGeneralDialog::ChooseGeneralDialog(const QStringList &general_names, QWidg
         }
     }
     foreach (const General *general, generals) {
-        QString caption = general->getTranslatedName();
+        QString caption;
+        if (general != NULL)
+            caption = general->getTranslatedName();
         OptionButton *button = new OptionButton(QString(), caption);
         if (no_icon) {
             button->setIcon(QIcon("image/system/no-general-icon.png"));
