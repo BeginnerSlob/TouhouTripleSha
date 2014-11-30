75aaf4266a68e7ef15594d325ceac7d93d5f05e0
BeginnerSlob <BeginnerSlob@gmail.com>
2014-11-30 16:39:23 +0800

    SNOW050 - 成濑万里亚

--- src/package/ikai-sui.h
+++ src/package/ikai-sui.h
@@ -232,6 +232,17 @@ public:
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
+class IkYaoyinCard: public SkillCard {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE IkYaoyinCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
 class IkChenyan: public TriggerSkill {
     Q_OBJECT
 
