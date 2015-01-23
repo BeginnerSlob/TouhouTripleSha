81f83e56a064c6ed969ed99f85d16b4270cf1891
BeginnerSlob <BeginnerSlob@gmail.com>
2015-01-23 19:02:56 +0800

    v8.1

--- lua/config.lua
+++ lua/config.lua
@@ -2,9 +2,9 @@
 -- and it is a little different from config.ini
 
 config = {
-	version = "v8.0",
+	version = "v8.1",
 	version_name = "东方绮符剧",
-	mod_name = "1227",
+	mod_name = "0123",
 	big_font = 56,
 	small_font = 27,
 	tiny_font = 18,
