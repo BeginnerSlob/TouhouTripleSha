79d0000d0bf25ba95d5004c7875121d7353112ec
BeginnerSlob <BeginnerSlob@gmail.com>
2015-02-20 20:50:39 +0800

    舰娘胜利条件

--- src/scenario/jianniang-scenario.cpp
+++ src/scenario/jianniang-scenario.cpp
@@ -19,6 +19,23 @@ public:
             break;
                         }
         case GameOverJudge: {
+            QList<ServerPlayer *> players = room->getAlivePlayers();
+            if (players.length() == 1)
+                room->gameOver(players.first()->objectName());
+            else {
+                QString first_role = players.first()->getRole();
+                if (first_role != "renegade") {
+                    bool all_same = true;
+                    foreach (ServerPlayer *p, players) {
+                        if (p->getRole() == "renegade" || p->getRole() != first_role) {
+                            all_same = false;
+                            break;
+                        }
+                    }
+                    if (all_same)
+                        room->gameOver(first_role);
+                }
+            }
             return true;
                             }
         case BuryVictim: {
