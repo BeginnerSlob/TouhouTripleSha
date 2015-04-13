973082e72cb8c0ac46090dc24f4f2eed0caef1fc
BeginnerSlob <BeginnerSlob@gmail.com>
2015-04-13 20:15:50 +0800

    SP016 - SP蕾米莉娅•斯卡雷特

--- src/package/touhou-sp.h
+++ src/package/touhou-sp.h
@@ -92,4 +92,15 @@ public:
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
+class ThFeihuCard : public SkillCard
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE ThFeihuCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
 #endif // TOUHOUSP_H
