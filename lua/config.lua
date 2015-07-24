8a26ba26325150557bb5856abd9c47a0a4de175f
BeginnerSlob <BeginnerSlob@gmail.com>
2015-07-24 22:55:27 +0800

    版本号

--- lua/config.lua
+++ lua/config.lua
@@ -2,9 +2,9 @@
 -- and it is a little different from config.ini
 
 config = {
-	version = "v8.1a",
+	version = "v8.3",
 	version_name = "东方绮符剧",
-	mod_name = "0126",
+	mod_name = "0725",
 	big_font = 56,
 	small_font = 27,
 	tiny_font = 18,
