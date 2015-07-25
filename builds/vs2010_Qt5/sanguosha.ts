1087131f35a156e018efbb5dbb08a8ce56f4c5c3
longaotianyisi <1171398010@qq.com>
2015-07-25 11:04:24 +0800

    Revert "……"
    This reverts commit 1701ce9ea9e7add499d5d483cbfd134efc204cfe.

--- builds/vs2010_Qt5/sanguosha.ts
+++ builds/vs2010_Qt5/sanguosha.ts
@@ -2691,34 +2691,34 @@
         <translation>&lt;font color=#EEB422&gt;玩家 &lt;b&gt;%1&lt;/b&gt; 加入了游戏&lt;/font&gt;</translation>
     </message>
     <message>
-        <location filename="../../src/server/room.cpp" line="2915"/>
+        <location filename="../../src/server/room.cpp" line="2921"/>
         <source>Unable to parse player</source>
         <translation>不可分析的玩家</translation>
     </message>
     <message>
-        <location filename="../../src/server/room.cpp" line="2917"/>
+        <location filename="../../src/server/room.cpp" line="2923"/>
         <source>Server is not waiting for reply from %1</source>
         <translation>服务器无法获取 %1 的响应</translation>
     </message>
     <message>
-        <location filename="../../src/server/room.cpp" line="2919"/>
+        <location filename="../../src/server/room.cpp" line="2925"/>
         <source>Reply command should be %1 instead of %2</source>
         <translation>应答指令应为 %1 而不是 %2</translation>
     </message>
     <message>
-        <location filename="../../src/server/room.cpp" line="2922"/>
+        <location filename="../../src/server/room.cpp" line="2928"/>
         <source>Reply serial should be %1 instead of %2</source>
         <translation>玩家序号应为 %1 而不是 %2</translation>
     </message>
     <message>
-        <location filename="../../src/server/room.cpp" line="4280"/>
+        <location filename="../../src/server/room.cpp" line="4286"/>
         <source>Card cannot be parsed:
  %1</source>
         <translation>卡牌字符串无法解析:
 %1</translation>
     </message>
     <message>
-        <location filename="../../src/server/room.cpp" line="5639"/>
+        <location filename="../../src/server/room.cpp" line="5645"/>
         <source>&lt;font color=#EEB422&gt;The network delay of player &lt;b&gt;%1&lt;/b&gt; is %2 milliseconds.&lt;/font&gt;</source>
         <translation>&lt;font color=#EEB422&gt;玩家 &lt;b&gt;%1&lt;/b&gt; 的网络延迟为 %2 毫秒&lt;/font&gt;</translation>
     </message>
