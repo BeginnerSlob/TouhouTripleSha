27759dd8e6df55ce0540c9f2fc46ec63b810f5c3
BeginnerSlob <BeginnerSlob@gmail.com>
2015-07-31 14:35:44 +0800

    WIND055 - 绝剑之舞 - 绀野木绵季

--- src/package/ikai-kin.h
+++ src/package/ikai-kin.h
@@ -71,6 +71,16 @@ public:
     virtual const Card *validate(CardUseStruct &cardUse) const;
 };
 
+class IkShengyongCard: public SkillCard {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE IkShengyongCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
 class IkJiushiCard: public SkillCard {
     Q_OBJECT
 
