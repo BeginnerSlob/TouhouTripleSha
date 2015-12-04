ba05b5b6e36039cbd4c8839a41f8e4b953b32b7b
BeginnerSlob <xindonger@gmail.com>
2015-12-04 17:33:01 +0800

    清兰

--- src/package/touhou-shin.h
+++ src/package/touhou-shin.h
@@ -29,4 +29,15 @@ public:
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
+class ThLiaoganCard : public SkillCard
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE ThLiaoganCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
 #endif // TOUHOUSHIN_H
