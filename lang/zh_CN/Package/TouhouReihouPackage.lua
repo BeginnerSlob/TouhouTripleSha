d32162f073a0ae36382aed9a78e7fb1f1c129f40
BeginnerSlob <xindonger@gmail.com>
2015-12-15 20:38:28 +0800

    灵宝init

--- lang/zh_CN/Package/TouhouReihouPackage.lua
+++ lang/zh_CN/Package/TouhouReihouPackage.lua
@@ -1,16 +1,16 @@
 -- translation for Reihou Package
 
 return {
-	["touhou-reihou"] = "东方的神祝",
+	["tenshi-reihou"] = "天子的灵宝",
 
-	["#kami005"]="三心菩提",
-	["kami005"]="七宝妙树",--灵 - 空 - 3血
-	["designer:kami005"]="幻桜落",
-	["illustrator:kami005"]="ms06s",
-	["cv:kami005"]="暂无",
-	["thfangcun"]="方寸",
-	[":thfangcun"]="准备阶段开始时，你可以从牌堆顶亮出X张牌（X为存活角色的数量，且至多为5），将其中任意数量的非锦囊牌以任意顺序置于牌堆底，然后将其余的牌置入弃牌堆。<br />※双击置入弃牌堆的牌，然后点击确定。",
-	["thhuifu"]="慧払",
-	[":thhuifu"]="出牌阶段限一次，你可将两张相同颜色的牌交给一名体力值比你多的角色，然后该角色须对其距离为1的由你指定的一名角色造成1点伤害。",
+	["#reihou005"]="三心菩提",
+	["reihou005"]="七宝妙树",--灵 - 空 - 3血
+	["designer:reihou005"]="幻桜落",
+	["illustrator:reihou005"]="ms06s",
+	["cv:reihou005"]="暂无",
+	["rhfangcun"]="方寸",
+	[":thfangcun"]="出牌阶段，你可以将一张黑色非锦囊牌当【心网密葬】使用。",
+	["rhhuifu"]="慧払",
+	[":thhuifu"]="锁定技，你不能成为【心网密葬】或【妙手探云】的目标。",
 
 }
