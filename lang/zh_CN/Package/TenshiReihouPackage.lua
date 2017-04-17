a3af3dc37dd38aa46db601afdba281fd25940d93
MomoHu <317995393@qq.com>
2017-04-17 13:50:23 -0700

    灵宝改动(少量技能名改动)
    大部分是 画师,卡名和称号,但是有少量技能名改动,需要注意

--- lang/zh_CN/Package/TenshiReihouPackage.lua
+++ lang/zh_CN/Package/TenshiReihouPackage.lua
@@ -6,7 +6,7 @@ return {
 	["#reihou001"] = "通天截影",
 	["reihou001"] = "金蛟剪",--灵 - 空 - 4血
 	["designer:reihou001"] = "名和行年",
-	["illustrator:reihou001"] = "[source]《討鬼伝2》鈴鹿御前",
+	["illustrator:reihou001"] = "ms06s",
 	["cv:reihou001"] = "暂无",
 	["rhduanlong"] = "断龙",
 	[":rhduanlong"] = "①当你失去“灵宝牌”时，你可以弃置一名角色装备区里的两张牌。②出牌阶段，你可以将“灵宝牌”置入灵宝牌堆。",
@@ -18,35 +18,35 @@ return {
 	["#reihou002"] = "三生万物",
 	["reihou002"] = "太上玲珑玉",--灵 - 空 - 4血
 	["designer:reihou002"] = "名和行年",
-	["illustrator:reihou002"] = "[source]《討鬼伝2》ツクヨミ",
+	["illustrator:reihou002"] = "ms06s",
 	["cv:reihou002"] = "暂无",
 	["rhruyi"] = "如意",
 	[":rhruyi"] = "当你需要使用任意基本牌或非延时类锦囊牌时，你可以将“灵宝牌”置入灵宝牌堆，视为使用此牌。",
 	["rhruyi_skill_saveself"] = "如意自救",
 	["rhruyi_skill_slash"] = "如意出杀",
 
-	["#reihou003"] = "相由心生",
+	["#reihou003"] = "千水一瞬",
 	["reihou003"] = "山河社稷图",--灵 - 空 - 4血
 	["designer:reihou003"] = "名和行年",
-	["illustrator:reihou003"] = "[source]《信長の野望ON》タクハタチヂヒメ",
+	["illustrator:reihou003"] = "ms06s",
 	["cv:reihou003"] = "暂无",
-	["rhhuanjing"] = "幻境",
+	["rhhuanjie"] = "幻界",
 	[":rhhuanjing"] = "①当此灵宝牌置于你的人物牌上后，你选择一种基本牌或非延时类锦囊牌。②当你需要使用你以此技能选择的牌时，你可以弃置一张黑桃牌，视为使用此牌。每回合限一次。",
 	["other_trick"] = "其他锦囊",
-	["#RhHuanjing"] = "%from 选择了 %arg",
+	["#RhHuanjie"] = "%from 选择了 %arg",
 
-	["#reihou004"] = "灭律丧音",
+	["#reihou004"] = "天地玄黄",
 	["reihou004"] = "东皇混沌钟",--灵 - 空 - 4血
 	["designer:reihou004"] = "名和行年",
-	["illustrator:reihou004"] = "[source]《討鬼伝2》クシナダヒメ",
+	["illustrator:reihou004"] = "ms06s",
 	["cv:reihou004"] = "暂无",
-	["rhlvcao"] = "律操",
-	[":rhlvcao"] = "锁定技。①当前回合角色使用锦囊牌无距离限制。②你不是当前回合角色使用黑色锦囊牌的合法目标。",
+	["rhhonghuang"] = "鸿荒",
+	[":rhhonghuang"] = "锁定技。①当前回合角色使用锦囊牌无距离限制。②你不是当前回合角色使用黑色锦囊牌的合法目标。",
 
 	["#reihou005"] = "三心菩提",
 	["reihou005"] = "七宝妙树",--灵 - 空 - 4血
 	["designer:reihou005"] = "名和行年",
-	["illustrator:reihou005"] = "[source]《討鬼伝》ヤマトヒメ",
+	["illustrator:reihou005"] = "ms06s",
 	["cv:reihou005"] = "暂无",
 	["rhliufu"] = "流払",
 	[":rhliufu"] = "当你成为基本牌或非延时类锦囊牌的目标后，你可以将“灵宝牌”置入灵宝牌堆，依次选择一至两项：1.令此牌对你无效；2.当此牌因结算结束而置入弃牌堆前，你将之交给一名角色。",
@@ -59,22 +59,22 @@ return {
 	["#reihou006"] = "接引西方极乐",
 	["reihou006"] = "十二品莲台",--灵 - 空 - 4血
 	["designer:reihou006"] = "名和行年",
-	["illustrator:reihou006"] = "[source]《信長の野望ON》イザナミ",
+	["illustrator:reihou006"] = "ms06s",
 	["cv:reihou006"] = "暂无",
 	["rhpujiu"] = "普救",
 	[":rhpujiu"] = "①你可以将一张手牌当【桃】使用。②当你以此法使用【桃】时，若此【桃】是你以此法使用的第X张牌（X为你的体力值），你将“灵宝牌”置入灵宝牌堆，然后你弃置所有手牌。",
 	["#rhpujiu"] = "普救（弃牌）",
 
-	["#reihou007"] = "无上杀伐道兵",
+	["#reihou007"] = "冥河自在",
 	["reihou007"] = "元屠",--灵 - 空 - 4血
 	["designer:reihou007"] = "名和行年",
-	["illustrator:reihou007"] = "[source]《信長の野望ON》摩利支天",
+	["illustrator:reihou007"] = "ms06s",
 	["cv:reihou007"] = "暂无",
 	["rhxuesha"] = "血杀",
 	[":rhxuesha"] = "锁定技。①当你受到伤害时，若你已受伤且此伤害不是你于当前回合内第一次受到的伤害，你防止此伤害。②你使用黑色【杀】无距离限制。③你使用的红色【杀】不是【闪】的合法目标。",
 
-	["#reihou008"] = "佛宝斗战胜",
-	["reihou008"] = "定海神针铁",--灵 - 空 - 4血
+	["#reihou008"] = "如意金箍",
+	["reihou008"] = "定海神珍铁",--灵 - 空 - 4血
 	["designer:reihou008"] = "名和行年",
 	["illustrator:reihou008"] = "[source]《信長の野望ON》タマヨリビメ",
 	["cv:reihou008"] = "暂无",
@@ -86,7 +86,7 @@ return {
 	["#reihou009"] = "知生注死",
 	["reihou009"] = "生死簿",--灵 - 空 - 4血
 	["designer:reihou009"] = "名和行年",
-	["illustrator:reihou009"] = "[source]《討鬼伝》伏姫",
+	["illustrator:reihou009"] = "ms06s",
 	["cv:reihou009"] = "暂无",
 	["rhgaiming"] = "改命",
 	[":rhgaiming"] = "阶段技。你可以选择一项：1.令一名体力值最大的角色失去1点体力；2.令一名体力值最小的角色回复1点体力。",
@@ -98,17 +98,17 @@ return {
 	["#reihou010"] = "索魂廿一日",
 	["reihou010"] = "钉头七箭书",--灵 - 空 - 4血
 	["designer:reihou010"] = "名和行年",
-	["illustrator:reihou010"] = "[source]《討鬼伝》瀧夜叉姫",
+	["illustrator:reihou010"] = "ms06s",
 	["cv:reihou010"] = "暂无",
 	["rhbaiming"] = "拜命",
 	[":rhbaiming"] = "当此灵宝牌置于你的人物牌上后，你选择一名其他角色，若如此做，其于你的“灵宝牌”置入灵宝牌堆前拥有“命”标记〈当你受到伤害后，拥有“<b>拜命</b>”的角色可以弃置一张牌，然后若你：有牌，其弃置你一张牌；没有牌，其令你失去1点体力〉。",
 	["@filth"] = "秽",
 	["@rhbaiming"] = "请选择“拜命”的目标",
 
-	["#reihou011"] = "驰骋九天",
-	["reihou011"] = "风火轮",--灵 - 空 - 4血
+	["#reihou011"] = "乾坤烁动",
+	["reihou011"] = "金霞风火轮",--灵 - 空 - 4血
 	["designer:reihou011"] = "名和行年",
-	["illustrator:reihou011"] = "[source]《討鬼伝2》アマテラス",
+	["illustrator:reihou011"] = "ms06s",
 	["cv:reihou011"] = "暂无",
 	["rhchengfeng"] = "乘风",
 	[":rhchengfeng"] = "你可以将一张黑色【杀】当【闪】使用或打出。",
@@ -118,7 +118,7 @@ return {
 	["#reihou012"] = "殒身成刃",
 	["reihou012"] = "干将",--灵 - 空 - 4血
 	["designer:reihou012"] = "名和行年",
-	["illustrator:reihou012"] = "[source]《信長の野望ON》アメノオハバリ",
+	["illustrator:reihou012"] = "ms06s",
 	["cv:reihou012"] = "暂无",
 	["rhzhengyang"] = "正阳",
 	[":rhzhengyang"] = "①当此灵宝牌置于你的人物牌上后，你可以将一张手牌置于人物牌上〔称为“阳”〕。②你使用的与“阳”花色相同的牌不是【闪】和【三粒天滴】的合法目标。③你不是其他角色使用与你的“阳”花色相同的牌的合法目标。④当你的“灵宝牌”置入灵宝牌堆后，你获取所有“阳”。",
@@ -129,7 +129,7 @@ return {
 	["#reihou013"] = "炼血铸兵",
 	["reihou013"] = "莫邪",--灵 - 空 - 4血
 	["designer:reihou013"] = "名和行年",
-	["illustrator:reihou013"] = "[source]《討鬼伝》甲斐姫",
+	["illustrator:reihou013"] = "ms06s",
 	["cv:reihou013"] = "暂无",
 	["rhchunyin"] = "纯阴",
 	[":rhchunyin"] = "①当此灵宝牌置于你的人物牌上后，你可以将一张手牌置于人物牌上〔称为“阴”〕。②你使用与“阴”花色相同的牌时，可以令一名其他角色摸一张牌。③其他角色使用与你的“阴”花色相同的牌时，你可以摸一张牌。④当你的“灵宝牌”置入灵宝牌堆后，你获取所有“阴”。",
@@ -138,17 +138,17 @@ return {
 	["@rhchunyin-draw"] = "你可以发动“纯阴”令一名其他角色摸一张牌",
 
 	["#reihou014"] = "燧人离荧",
-	["reihou014"] = "五火七禽扇",--灵 - 空 - 4血
+	["reihou014"] = "五火七翎扇",--灵 - 空 - 4血
 	["designer:reihou014"] = "名和行年",
-	["illustrator:reihou014"] = "[source]《信長の野望ON》アメノウズメ",
+	["illustrator:reihou014"] = "ms06s",
 	["cv:reihou014"] = "暂无",
 	["rhsanmei"] = "三昧",
 	[":rhsanmei"] = "锁定技。①所有角色红色牌造成的伤害均为火属性伤害。②当你受到火属性伤害时，你防止此伤害。",
 
-	["#reihou015"] = "显圣灌江口",
-	["reihou015"] = "三尖两刃刀",--灵 - 空 - 4血
+	["#reihou015"] = "清源妙道",
+	["reihou015"] = "三尖两刃神锋",--灵 - 空 - 4血
 	["designer:reihou015"] = "名和行年",
-	["illustrator:reihou015"] = "[source]《討鬼伝2》巴御前",
+	["illustrator:reihou015"] = "ms06s",
 	["cv:reihou015"] = "暂无",
 	["rhxuanren"] = "旋刃",
 	[":rhxuanren"] = "一名角色受到【杀】造成的伤害后，你可以弃置一张手牌，对其距离为1的一名其他角色造成1点伤害。",
@@ -156,9 +156,9 @@ return {
 	["~rhxuanren"] = "选择一张手牌→选择目标→点击“确定”",
 
 	["#reihou016"] = "凝陆挈海",
-	["reihou016"] = "天琼鉾",--灵 - 空 - 4血
+	["reihou016"] = "天沼矛",--灵 - 空 - 4血
 	["designer:reihou016"] = "名和行年",
-	["illustrator:reihou016"] = "[source]《討鬼伝2》ククリヒメ",
+	["illustrator:reihou016"] = "ms06s",
 	["cv:reihou016"] = "暂无",
 	["rhguozao"] = "国造",
 	[":rhguozao"] = "①当此灵宝牌置于你的人物牌上后，你加1点体力上限，然后回复1点体力。②当你的“灵宝牌”置入灵宝牌堆后，你减1点体力上限，然后若你的体力因“国造②”而减少，你摸一张牌。",
@@ -166,26 +166,26 @@ return {
 	["#reihou017"] = "神光天岩户",
 	["reihou017"] = "八咫镜",--灵 - 空 - 4血
 	["designer:reihou017"] = "名和行年",
-	["illustrator:reihou017"] = "[source]《討鬼伝》アマテラス",
+	["illustrator:reihou017"] = "ms06s",
 	["cv:reihou017"] = "暂无",
 	["rhshenguang"] = "神光",
 	[":rhshenguang"] = "一名角色的摸牌阶段开始时，你可以将“灵宝牌”置入灵宝牌堆，亮出牌堆顶的X张牌（X为角色数），然后所有角色可以对该角色使用【杀】（无距离限制），若如此做，其获取这些牌中的一张牌。",
 	["@rhshenguang"] = "你可以对 %src 使用【杀】",
 
 	["#reihou018"] = "宫箦荒魂",
-	["reihou018"] = "天丛云剑",--灵 - 空 - 4血
+	["reihou018"] = "天丛云",--灵 - 空 - 4血
 	["designer:reihou018"] = "名和行年",
-	["illustrator:reihou018"] = "[source]《討鬼伝》ミヤズヒメ",
+	["illustrator:reihou018"] = "ms06s",
 	["cv:reihou018"] = "暂无",
 	["rhmangti"] = "芒薙",
 	[":rhmangti"] = "①你可以将一张梅花牌当【心网密葬】使用。②手牌数小于2的其他角色视为在你的攻击范围内。",
 	["rhlingwei"] = "灵卫",
 	[":rhlingwei"] = "你不是【心网密葬】和【妙手探云】的合法目标。",
 
-	["#reihou019"] = "雉鸣问初心",
+	["#reihou019"] = "雉鸣问弦",
 	["reihou019"] = "天鹿児弓",--灵 - 空 - 4血
 	["designer:reihou019"] = "名和行年",
-	["illustrator:reihou019"] = "[source]《討鬼伝》板額御前",
+	["illustrator:reihou019"] = "ms06s",
 	["cv:reihou019"] = "暂无",
 	["rhwangzhong"] = "忘衷",
 	[":rhwangzhong"] = "一名角色的出牌阶段结束时，若你的手牌数小于体力上限，你可以摸一张牌，然后你需对该角色使用【杀】（无距离限制），否则你失去1点体力。",
@@ -194,7 +194,7 @@ return {
 	["#reihou020"] = "神剑平国",
 	["reihou020"] = "布都御魂",--灵 - 空 - 4血
 	["designer:reihou020"] = "名和行年",
-	["illustrator:reihou020"] = "[source]《討鬼伝2》甲斐姫",
+	["illustrator:reihou020"] = "ms06s",
 	["cv:reihou020"] = "暂无",
 	["rhcuigu"] = "摧骨",
 	[":rhcuigu"] = "当你于出牌阶段内使用【杀】指定一个目标后，可以将“灵宝牌”置入灵宝牌堆，令其于此回合内不能使用【桃】且不是【桃】的合法目标。",
@@ -202,10 +202,10 @@ return {
 	["rhlinghai"] = "灵骸",
 	[":rhlinghai"] = "当一名角色受到伤害时，若伤害值不小于其体力值，你可以将“灵宝牌”置入灵宝牌堆，防止此伤害。",
 
-	["#reihou021"] = "烈枪豪杰",
+	["#reihou021"] = "绝风烈刃",
 	["reihou021"] = "蜻蛉切",--灵 - 空 - 4血
 	["designer:reihou021"] = "名和行年",
-	["illustrator:reihou021"] = "[source]《討鬼伝2》坂本乙女",
+	["illustrator:reihou021"] = "ms06s",
 	["cv:reihou021"] = "暂无",
 	["rhhaoqiang"] = "豪枪",
 	[":rhhaoqiang"] = "你可以将一张锦囊牌当【杀】或【碎月绮斗】使用或打出。每回合限一次。",
@@ -216,7 +216,7 @@ return {
 	["#reihou022"] = "歌仙宿光",
 	["reihou022"] = "和泉守兼定",--灵 - 空 - 4血
 	["designer:reihou022"] = "名和行年",
-	["illustrator:reihou022"] = "[source]《信長の野望ON》サホヒメ",
+	["illustrator:reihou022"] = "ms06s",
 	["cv:reihou022"] = "暂无",
 	["rhyaren"] = "雅刃",
 	[":rhyaren"] = "阶段技。你可以与一名角色拼点：当你赢后，你到该角色的距离于此回合内视为1；当你没赢后，你可以令其回复1点体力。",
@@ -224,10 +224,10 @@ return {
 	["rhshixiang"] = "诗响",
 	[":rhshixiang"] = "其他角色的回合开始时，若你此前没有发动过“<b>雅刃</b>”，你可以将“灵宝牌”置入灵宝牌堆，令其获得于此回合内的“<b>雅刃</b>”。",
 
-	["#reihou023"] = "临战起征声",
+	["#reihou023"] = "心流天斩",
 	["reihou023"] = "长曾弥虎彻",--灵 - 空 - 4血
 	["designer:reihou023"] = "名和行年",
-	["illustrator:reihou023"] = "[source]《信長の野望ON》カムオオイチヒメ",
+	["illustrator:reihou023"] = "ms06s",
 	["cv:reihou023"] = "暂无",
 	["rhyinren"] = "寅刃",
 	[":rhyinren"] = "①出牌阶段，你可以将两张手牌交给一名角色，视为对其使用【杀】（无距离限制）。②当你第三次发动“<b>寅刃</b>①”时，你将“灵宝牌”置入灵宝牌堆，然后失去1点体力。",
@@ -237,7 +237,7 @@ return {
 	["#reihou024"] = "血嗜第六天",
 	["reihou024"] = "千子村正",--灵 - 空 - 4血
 	["designer:reihou024"] = "名和行年",
-	["illustrator:reihou024"] = "[source]《信長の野望ON》カネヤマヒメ",
+	["illustrator:reihou024"] = "ms06s",
 	["cv:reihou024"] = "暂无",
 	["rhyiqie"] = "刈切",
 	[":rhyiqie"] = "当你使用【杀】指定一个目标后，可以令其弃置装备区里的一张牌。",
@@ -245,70 +245,70 @@ return {
 	[":rhyaozhang"] = "当你成为【杀】的目标时，使用者需弃置一张装备牌，否则此【杀】对你无效。",
 	["@rhyaozhang"] = "%src 发动了“妖障”，请弃置一张装备牌，否则此【杀】对其无效",
 
-	["#reihou025"] = "梦幻泡影",
-	["reihou025"] = "般若鬼面",--灵 - 空 - 4血
+	["#reihou025"] = "狂言舞事",
+	["reihou025"] = "般若禅面",--灵 - 空 - 4血
 	["designer:reihou025"] = "名和行年",
-	["illustrator:reihou025"] = "[source]《信長の野望ON》ウカノミタマ",
+	["illustrator:reihou025"] = "ms06s",
 	["cv:reihou025"] = "暂无",
 	["rhsanglv"] = "丧律",
 	[":rhsanglv"] = "一名角色的弃牌阶段结束时，若其未于出牌阶段内造成过伤害且若其于弃牌阶段内弃置过【杀】，你可以令其视为使用【杀】。",
 
-	["#reihou026"] = "叶隐幻化",
+	["#reihou026"] = "叶隐黄泉",
 	["reihou026"] = "万川集海",--灵 - 空 - 4血
 	["designer:reihou026"] = "名和行年",
-	["illustrator:reihou026"] = "[source]《討鬼伝》望月千代女",
+	["illustrator:reihou026"] = "ms06s",
 	["cv:reihou026"] = "暂无",
 	["rhwuyin"] = "雾隐",
 	[":rhwuyin"] = "一名角色的出牌阶段开始时，你可以令其视为对你选择的另一名角色使用【罔两空界】。",
 	["@rhwuyin"] = "你可以发动“雾隐”",
 
-	["#reihou027"] = "本来无一物",
-	["reihou027"] = "燕之子安贝",--灵 - 空 - 4血
+	["#reihou027"] = "护羽姬",
+	["reihou027"] = "燕子安贝",--灵 - 空 - 4血
 	["designer:reihou027"] = "名和行年",
-	["illustrator:reihou027"] = "[source]《信長の野望ON》アメノトリフネ",
+	["illustrator:reihou027"] = "ms06s",
 	["cv:reihou027"] = "暂无",
 	["rhchanling"] = "产灵",
 	[":rhchanling"] = "当你距离不大于1的一名角色受到伤害后，你可以摸X张牌（X为此伤害的伤害值），然后你可以将等量的手牌交给该角色。",
 	["@rhchanling"] = "你可以交给 %src %arg张手牌",
 
-	["#reihou028"] = "睹物思月姬",
+	["#reihou028"] = "蓬華宝土",
 	["reihou028"] = "佛御石钵",--灵 - 空 - 4血
 	["designer:reihou028"] = "名和行年",
-	["illustrator:reihou028"] = "[source]《討鬼伝2》鉢かづき",
+	["illustrator:reihou028"] = "ms06s",
 	["cv:reihou028"] = "暂无",
 	["rhshendai"] = "身代",
 	[":rhshendai"] = "当一名角色因获取或另一名角色的弃置而失去手牌前，你可以将“灵宝牌”置入灵宝牌堆，终止移动流程。",
 
-	["#reihou029"] = "痴心妄想",
-	["reihou029"] = "龙颈之玉",--灵 - 空 - 4血
+	["#reihou029"] = "五色妄心",
+	["reihou029"] = "龙颈玉",--灵 - 空 - 4血
 	["designer:reihou029"] = "名和行年",
-	["illustrator:reihou029"] = "[source]《信長の野望ON》アメノサグメ",
+	["illustrator:reihou029"] = "ms06s",
 	["cv:reihou029"] = "暂无",
 	["rhjiyu"] = "觊觎",
 	[":rhjiyu"] = "一名角色的回合结束时，若该角色于此回合内对你使用过牌，且若其未于此回合内对你造成过伤害，你可以摸一张牌。",
 
-	["#reihou030"] = "长生之祝",
-	["reihou030"] = "望月盃",--灵 - 空 - 4血
+	["#reihou030"] = "不死大罪",
+	["reihou030"] = "蓬莱之藥",--灵 - 空 - 4血
 	["designer:reihou030"] = "名和行年",
-	["illustrator:reihou030"] = "[source]《信長の野望ON》ワクムスビ",
+	["illustrator:reihou030"] = "ms06s",
 	["cv:reihou030"] = "暂无",
-	["rhfuyin"] = "赋饮",
-	[":rhfuyin"] = "一名角色的出牌阶段开始时，你可以将“灵宝牌”置入灵宝牌堆，令其选择一项：1.视为使用【桃】；2.视为使用【酒】（计入限制的使用次数）。",
-	["rhfuyin:peach"] = "视为使用【桃】",
-	["rhfuyin:analeptic"] = "视为使用【酒】",
+	["rhjinbei"] = "禁盃",
+	[":rhjinbei"] = "一名角色的出牌阶段开始时，你可以将“灵宝牌”置入灵宝牌堆，令其选择一项：1.视为使用【桃】；2.视为使用【酒】（计入限制的使用次数）。",
+	["rhjinbei:peach"] = "视为使用【桃】",
+	["rhjinbei:analeptic"] = "视为使用【酒】",
 
-	["#reihou031"] = "黑鹰阴羽",
-	["reihou031"] = "灭弓毗纳嘉",--灵 - 空 - 4血
+	["#reihou031"] = "却邪灭弓",
+	["reihou031"] = "毗纳嘉",--灵 - 空 - 4血
 	["designer:reihou031"] = "名和行年",
-	["illustrator:reihou031"] = "[source]《討鬼伝2》板額御前",
+	["illustrator:reihou031"] = "ms06s",
 	["cv:reihou031"] = "暂无",
 	["rhpihuai"] = "圮坏",
 	[":rhpihuai"] = "当一名其他角色受到伤害后，若其在你的攻击范围外，你可以弃置其一张手牌。",
 
-	["#reihou032"] = "金身不坏",
-	["reihou032"] = "金刚杵伐折罗",--灵 - 空 - 4血
+	["#reihou032"] = "金刚杵",
+	["reihou032"] = "伐折罗",--灵 - 空 - 4血
 	["designer:reihou032"] = "名和行年",
-	["illustrator:reihou032"] = "[source]《討鬼伝2》茨木童子",
+	["illustrator:reihou032"] = "ms06s",
 	["cv:reihou032"] = "暂无",
 	["rhdangmo"] = "荡魔",
 	[":rhdangmo"] = "当你使用【杀】指定一个目标后，你可以令其非锁定技于当前回合内无效。",
@@ -318,15 +318,15 @@ return {
 	["#reihou033"] = "一梦如是",
 	["reihou033"] = "三子舍利",--灵 - 空 - 4血
 	["designer:reihou033"] = "名和行年",
-	["illustrator:reihou033"] = "[source]《討鬼伝2》コノハナサクヤヒメ",
+	["illustrator:reihou033"] = "ms06s",
 	["cv:reihou033"] = "暂无",
 	["rhnieji"] = "涅寂",
 	[":rhnieji"] = "一名角色进入濒死状态时，你可以将“灵宝牌”置入灵宝牌堆，令其将人物牌恢复至游戏开始时的状态，然后将体力值回复至1点。",
 
-	["#reihou034"] = "紫微九色天",
-	["reihou034"] = "五色乱星",--灵 - 空 - 4血
+	["#reihou034"] = "九天游星",
+	["reihou034"] = "紫地罗板五色乱星",--灵 - 空 - 4血
 	["designer:reihou034"] = "名和行年",
-	["illustrator:reihou034"] = "[source]《討鬼伝》イザナミ",
+	["illustrator:reihou034"] = "ms06s",
 	["cv:reihou034"] = "暂无",
 	["rhshenluo"] = "神罗",
 	[":rhshenluo"] = "当此灵宝牌置于你的人物牌上后，你可以选择一名其他角色，若如此做，其于你的“灵宝牌”置入灵宝牌堆前拥有“宸”标记〈当一名其他角色受到伤害后，你回复1点体力〉。",
@@ -336,7 +336,7 @@ return {
 	["#reihou035"] = "削褫三五",
 	["reihou035"] = "混元金斗",--灵 - 空 - 4血
 	["designer:reihou035"] = "名和行年",
-	["illustrator:reihou035"] = "[source]《信長の野望ON》コノハナサクヤヒメ",
+	["illustrator:reihou035"] = "ms06s",
 	["cv:reihou035"] = "暂无",
 	["rhyousheng"] = "由生",
 	[":rhyousheng"] = "当你使用牌指定目标后，或成为牌的目标后，你可以将一张手牌当任意延时类锦囊牌使用。每回合限一次。",
@@ -346,25 +346,25 @@ return {
 	["#reihou036"] = "吉野白拍子",
 	["reihou036"] = "冰绡云舞扇",--灵 - 空 - 4血
 	["designer:reihou036"] = "名和行年",
-	["illustrator:reihou036"] = "[source]《討鬼伝2》お市",
+	["illustrator:reihou036"] = "ms06s",
 	["cv:reihou036"] = "暂无",
 	["rhjianuo"] = "佳傩",
 	[":rhjianuo"] = "一名角色的出牌阶段开始时，你可以将“灵宝牌”置入灵宝牌堆，令其于此回合内拥有“魅”标记〈当你弃置一张牌前，拥有“<b>佳傩</b>”的角色可以令一名其他角色获得之〉。",
 
-	["#reihou037"] = "天安川之誓",
+	["#reihou037"] = "神户经纬",
 	["reihou037"] = "大叶刈",--灵 - 空 - 4血
 	["designer:reihou037"] = "名和行年",
-	["illustrator:reihou037"] = "[source]《信長の野望ON》ワカヒルメ",
+	["illustrator:reihou037"] = "ms06s",
 	["cv:reihou037"] = "暂无",
 	["rhyizhi"] = "易质",
 	[":rhyizhi"] = "出牌阶段，你可以失去1点体力，将一名角色装备区里的一张牌置入另一名角色的相应区域。",
 	["rhguozhu"] = "国逐",
 	[":rhguozhu"] = "当你受到伤害后，你可以回复1点体力，然后发动“<b>易质</b>”。",
 
-	["#reihou038"] = "龙宫的馈赠",
+	["#reihou038"] = "乙姬之馈",
 	["reihou038"] = "玉手箱",--灵 - 空 - 4血
 	["designer:reihou038"] = "名和行年",
-	["illustrator:reihou038"] = "[source]《討鬼伝》乙姫",
+	["illustrator:reihou038"] = "ms06s",
 	["cv:reihou038"] = "暂无",
 	["rhshiguang"] = "时光",
 	[":rhshiguang"] = "①当此灵宝牌置于你的人物牌上后，你获得4枚“时”标记〈出牌阶段，你可以弃1枚“时”标记，摸一张牌，你的手牌上限于此回合内-1〉。②当你的“灵宝牌”置入灵宝牌堆后，你弃场上的所有“时”标记。",
@@ -372,17 +372,17 @@ return {
 	[":rhkuili"] = "一名角色的回合开始时，你可以弃任意枚“时”标记，该角色获得等量“时”标记。",
 
 	["#reihou039"] = "十拳迦毛大御",
-	["reihou039"] = "神度剑",--灵 - 空 - 4血
+	["reihou039"] = "神度",--灵 - 空 - 4血
 	["designer:reihou039"] = "名和行年",
-	["illustrator:reihou039"] = "[source]《信長の野望ON》タギリヒメ",
+	["illustrator:reihou039"] = "ms06s",
 	["cv:reihou039"] = "暂无",
 	["rhchensheng"] = "辰升",
 	[":rhchensheng"] = "出牌阶段，或当你受到伤害后，你可以将“灵宝牌”置入灵宝牌，视为使用【荒波暴流】。",
 
 	["#reihou040"] = "大所高所物灵",
-	["reihou040"] = "白狼判官笔",--灵 - 空 - 4血
+	["reihou040"] = "白理判官笔",--灵 - 空 - 4血
 	["designer:reihou040"] = "名和行年",
-	["illustrator:reihou040"] = "[source]《信長の野望ON》トヨタマヒメ",
+	["illustrator:reihou040"] = "ms06s",
 	["cv:reihou040"] = "暂无",
 	["rhxinmo"] = "新墨",
 	[":rhxinmo"] = "当你失去一张红色牌后，你可以令一名角色摸一张牌，然后你将人物牌恢复至游戏开始时的状态。每回合限一次。",
@@ -390,15 +390,15 @@ return {
 	["#reihou041"] = "逝故沙恭达罗",
 	["reihou041"] = "浊水葬",--灵 - 空 - 4血
 	["designer:reihou041"] = "名和行年",
-	["illustrator:reihou041"] = "[source]《信長の野望ON》タギツヒメ",
+	["illustrator:reihou041"] = "ms06s",
 	["cv:reihou041"] = "暂无",
 	["rhfuyu"] = "浮屿",
 	[":rhfuyu"] = "一名角色的出牌阶段开始时，你可以选择一项：1.令其到其他角色的距离于此回合内-1；2.令其到其他角色的距离于此回合内+1。",
 
-	["#reihou042"] = "惑纣贵人",
-	["reihou042"] = "玉石琵琶",--灵 - 空 - 4血
+	["#reihou042"] = "四弦四相",
+	["reihou042"] = "东尊宝慧琵琶",--灵 - 空 - 4血
 	["designer:reihou042"] = "名和行年",
-	["illustrator:reihou042"] = "[source]《信長の野望ON》モモソヒメ",
+	["illustrator:reihou042"] = "ms06s",
 	["cv:reihou042"] = "暂无",
 	["rhyaodao"] = "妖祷",
 	[":rhyaodao"] = "①当此灵宝牌置于你的人物牌上后，你可以令一名角色将灵宝牌堆里的一张随机牌置于人物牌上〔称为“灵宝牌”〕，其于你的“灵宝牌”置入灵宝牌堆前拥有“灵宝牌”上的技能。②当你的“灵宝牌”置入灵宝牌堆后，你将场上的所有“灵宝牌”置入灵宝牌堆。",
@@ -406,7 +406,7 @@ return {
 	["#reihou043"] = "慈悲观自在",
 	["reihou043"] = "杨柳玉净瓶",--灵 - 空 - 4血
 	["designer:reihou043"] = "名和行年",
-	["illustrator:reihou043"] = "[source]《信長の野望ON》ヤガミヒメ",
+	["illustrator:reihou043"] = "ms06s",
 	["cv:reihou043"] = "暂无",
 	["rhchuilu"] = "垂露",
 	[":rhchuilu"] = "①你可以将一张红色牌当【桃】使用。②出牌阶段，你能对其他角色使用【桃】。",
@@ -414,7 +414,7 @@ return {
 	["#reihou044"] = "络缲机巧",
 	["reihou044"] = "茶运人形",--灵 - 空 - 4血
 	["designer:reihou044"] = "名和行年",
-	["illustrator:reihou044"] = "[source]《討鬼伝》壱與",
+	["illustrator:reihou044"] = "ms06s",
 	["cv:reihou044"] = "暂无",
 	["rhxianming"] = "献茗",
 	[":rhxianming"] = "①当此灵宝牌置于你的人物牌上后，你可以将任意张手牌置于人物牌上〔称为“茗”〕。②一名角色于出牌阶段内使用【杀】指定目标后，你可以将一张“茗”置入弃牌堆，令此【杀】不计入限制的使用次数。③当你的“灵宝牌”置入灵宝牌堆后，你将所有“茗”置入弃牌堆。",
@@ -422,7 +422,7 @@ return {
 	["#reihou045"] = "先天无尽藏",
 	["reihou045"] = "九天息壤",--灵 - 空 - 4血
 	["designer:reihou045"] = "名和行年",
-	["illustrator:reihou045"] = "[source]《信長の野望ON》クニノミハシラ",
+	["illustrator:reihou045"] = "ms06s",
 	["cv:reihou045"] = "暂无",
 	["rhxiaozhang"] = "消长",
 	[":rhxiaozhang"] = "①当此灵宝牌置于你的人物牌上后，你将牌堆顶的X张牌置于人物牌上（X为角色数）〔称为“壤”〕。②一名角色的结束阶段开始时，其可以弃置一张牌，获取一张“壤”。③当你的“灵宝牌”置入灵宝牌堆后，你可以获取一至两张“壤”，然后将所有“壤”置入弃牌堆。",
@@ -430,7 +430,7 @@ return {
 	["#reihou046"] = "拳印执金刚",
 	["reihou046"] = "鬼童丸",--灵 - 空 - 4血
 	["designer:reihou046"] = "名和行年",
-	["illustrator:reihou046"] = "[source]《信長の野望ON》オオヤツヒメ",
+	["illustrator:reihou046"] = "ms06s",
 	["cv:reihou046"] = "暂无",
 	["rhfapo"] = "发破",
 	[":rhfapo"] = "你于出牌阶段使用【杀】能额外选择任意名手牌多于你的角色为目标。每阶段限一次。",
@@ -438,23 +438,23 @@ return {
 	[":rhhujuan"] = "锁定技。当你受到的伤害结算开始时/失去体力前，你防止此伤害/终止此失去体力结算。",
 
 	["#reihou047"] = "戮火迦具土",
-	["reihou047"] = "天之尾羽张",--灵 - 空 - 4血
+	["reihou047"] = "天尾羽张",--灵 - 空 - 4血
 	["designer:reihou047"] = "名和行年",
-	["illustrator:reihou047"] = "[source]《信長の野望ON》コノハナサクヤヒメ",
+	["illustrator:reihou047"] = "ms06s",
 	["cv:reihou047"] = "暂无",
 	["rhdanshen"] = "诞神",
 	[":rhdanshen"] = "当一名角色造成伤害后，若受到伤害的角色的体力值小于该角色，你可以摸一张牌，然后将一张牌交给该角色。",
 
 	["#reihou048"] = "囹圄切羽诘",
-	["reihou048"] = "捆仙绳",--灵 - 空 - 4血
+	["reihou048"] = "梱仙绳",--灵 - 空 - 4血
 	["designer:reihou048"] = "名和行年",
-	["illustrator:reihou048"] = "[source]《信長の野望ON》クシナダヒメ",
+	["illustrator:reihou048"] = "ms06s",
 	["cv:reihou048"] = "暂无",
 	["rhzhangchi"] = "张弛",
 	[":rhzhangchi"] = "当此灵宝牌置于你的人物牌上后，你可以选择一名角色，若如此做，其于你的“灵宝牌”置入灵宝牌堆前拥有“结”标记〈①你不是你上家和下家使用牌的合法目标。②你使用的牌可以跳过合法性检测选择上家或下家为目标〉。",
 
 	["#reihou049"] = "张悬京极萤",
-	["reihou049"] = "兔耳前立兜",--灵 - 空 - 4血
+	["reihou049"] = "银箔押兔耳前立兜",--灵 - 空 - 4血
 	["designer:reihou049"] = "名和行年",
 	["illustrator:reihou049"] = "[source]《信長の野望ON》アカルヒメ",
 	["cv:reihou049"] = "暂无",
