4218737ed8b15b8b384357fb3e9f1d51aef1b546
BeginnerSlob <slob@mogara.org>
2018-04-27 20:34:42 +0800

    版本号

--- lua/config.lua
+++ lua/config.lua
@@ -2,9 +2,9 @@
 -- and it is a little different from config.ini
 
 config = {
-	version = "v8.9.2",
-	resource_version = "v8.9.2.0",
-	version_name = "0425",
+	version = "v8.9.3",
+	resource_version = "v8.9.3.0",
+	version_name = "0427",
 	mod_name = "TouhouTripleSha",
 	big_font = 56,
 	small_font = 27,
