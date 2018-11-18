c57395c63941cb7ccaec0467b14c29646515b734
Leoongithub <hjq447285628@163.com>
2018-11-18 19:09:51 +0800

    fix #263
    test

--- src/package/touhou-tsuki.cpp
+++ src/package/touhou-tsuki.cpp
@@ -1604,7 +1604,7 @@ void ThKanyaoCard::use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &
             }
         if (unsame) {
             QList<ServerPlayer *> victims;
-            foreach (ServerPlayer *p, room->getOtherPlayers(source))
+            foreach (ServerPlayer *p, room->getAlivePlayers(source))
                 if (p->getHp() >= source->getHp())
                     victims << p;
             if (!victims.isEmpty()) {
