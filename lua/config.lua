5d55bcd89ef6e71a630a3e53aa540a6d5980e8fa
BeginnerSlob <slob@mogara.org>
2021-03-23 12:21:45 +0800

    v9.0

--- lua/config.lua
+++ lua/config.lua
@@ -2,9 +2,9 @@
 -- and it is a little different from config.ini
 
 config = {
-	version = "v8.9.9",
-	resources_version = "v8.9.9.0",
-	version_name = "0525",
+	version = "v9.0",
+	resources_version = "v9.0",
+	version_name = "0323",
 	mod_name = "TouhouTripleSha",
 	big_font = 56,
 	small_font = 27,
